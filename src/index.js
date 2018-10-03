f55ae88ddfe751039af5b97a34798df74789ba8c
Michael <mbg@outlook.com>
2018-10-04 00:32:00 +0100

    Update

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Box from './components/Box.js'
+import Button from './components/Button.js'
+import Checkbox from './components/Checkbox.js'
+import Input from './components/Input.js'
+import Select from './components/Select.js'
+import Tab from './components/Tab.js'
+import TabList from './components/TabList.js'
+import TabPanels from './components/TabPanels.js'
+import Tabs from './components/Tabs.js'
+import Text from './components/Text.js'
+
+export {
+  Box,
+  Button,
+  Checkbox,
+  Input,
+  Select,
+  Tab,
+  TabList,
+  TabPanels,
+  Tabs,
+  Text
+}
